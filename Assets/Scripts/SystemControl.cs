260739bdbf2815090f40c60829951ead355c8b4d
Ayuki2008 <103936763+Ayuki2008@users.noreply.github.com>
2022-07-20 21:34:48 +0800

    LearnLoop

--- Assets/Scripts/SystemControl.cs
+++ Assets/Scripts/SystemControl.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 //命名空間 namespace 空間名稱 { 該空間的內容 }
 namespace SH
@@ -22,11 +23,20 @@ namespace SH
         public Transform traSpawnPoint;
         [Header("攻擊參數名稱")]
         public string parAttack = "觸發攻擊";
+        [Header("彈珠發射速度"), Range(0, 5000)]
+        public float speedBall = 1000;
+        [Header("彈珠發射間隔"), Range(0, 2)]
+        public float intervalBall = 0.5f;
 
         public Animator ani;
         #endregion
 
         #region 事件
+        // Awake 在 Start 之前執行一次
+        private void Awake()
+        {
+            ani = GetComponent<Animator>();    
+        }
         private void Update()
         {
             ShootBall();
@@ -47,16 +57,40 @@ namespace SH
         /// </summary> 
         private void ShootBall()
         {
-            //放開 滑鼠左鍵 生成並發射彈珠
-            if (Input.GetKeyUp(KeyCode.Mouse0))//Mouse1右 Mouse2中
+            // 按下 滑鼠左鍵 顯示 箭頭
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                arrow.SetActive(true);
+            }
+            // 放開 滑鼠左鍵 生成並發射彈珠
+            else if (Input.GetKeyUp(KeyCode.Mouse0))//Mouse1右 Mouse2中
 
             {
                 print("放開左鍵！");
+                arrow.SetActive(false);
+                StartCoroutine(SpawnBall());
+                
+            }
+        }
+        /// <summary>
+        /// 生成彈珠附帶間隔時間
+        /// </summary>
+        private IEnumerator SpawnBall()
+        {
+            for (int i = 0; i < canShootBallTotal; i++)
+            {
+                ani.SetTrigger(parAttack);
 
                 // Object 類別可省略不寫
                 // 直接透過 Object 成員名稱使用
                 // 生成 彈珠;
-                Instantiate(ball, traSpawnPoint.position, Quaternion.identity);
+                // Ouaternion.identity 零度角
+                GameObject tempBall = Instantiate(ball, traSpawnPoint.position, Quaternion.identity);
+                // 暫存彈珠 取得鋼體元件 添加推力 (角色.前方 * 速度)
+                // transform.forward 角色的前方
+                tempBall.GetComponent<Rigidbody>().AddForce(transform.forward * speedBall);
+
+                yield return new WaitForSeconds(intervalBall);
             }
         }
 
